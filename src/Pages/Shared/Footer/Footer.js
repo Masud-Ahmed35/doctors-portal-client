cbd45544f54f83a089ff43b942edc165e6a6eecd
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-11-13 04:33:18 +0600

    add footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import footer from '../../../assets/images/footer.png';
 
 const Footer = () => {
     return (
-        <div>
-            <h3>This is footer</h3>
-        </div>
+        <footer className="p-10 mt-14"
+            style={{
+                background: `url(${footer})`,
+                backgroundSize: 'cover',
+            }}
+        >
+            <div className='flex flex-col text-center md:flex-row md:justify-around'>
+                <div className='flex flex-col'>
+                    <span className="footer-title">Services</span>
+                    <Link to='/' className="link link-hover">Branding</Link>
+                    <Link to='/' className="link link-hover">Design</Link>
+                    <Link to='/' className="link link-hover">Marketing</Link>
+                    <Link to='/' className="link link-hover">Advertisement</Link>
+                </div>
+                <div className='flex flex-col'>
+                    <span className="footer-title">Company</span>
+                    <Link to='/' className="link link-hover">About us</Link>
+                    <Link to='/' className="link link-hover">Contact</Link>
+                    <Link to='/' className="link link-hover">Jobs</Link>
+                    <Link to='/' className="link link-hover">Press kit</Link>
+                </div>
+                <div className='flex flex-col'>
+                    <span className="footer-title">Legal</span>
+                    <Link to='/' className="link link-hover">Terms of use</Link>
+                    <Link to='/' className="link link-hover">Privacy policy</Link>
+                    <Link to='/' className="link link-hover">Cookie policy</Link>
+                </div>
+            </div>
+            <footer className="footer footer-center p-4 mt-14">
+                <div>
+                    <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+                </div>
+            </footer>
+        </footer>
     );
 };
 
